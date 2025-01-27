2c4e959657b13c9580db8bb429f5c96534cea09c
Shun Ota <sota@student.42tokyo.jp>
2025-01-27 16:21:06 +0900

    feat: add nix install script

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+INSTALL_DIR="/goinfre/$USER/nix"
+BINARY="$INSTALL_DIR/nix"
+
+VERSION="2.20"
+if [[ -n "$1" ]]; then
+	VERSION="$1"
+fi
+
+if [[ -d "$INSTALL_DIR" ]]; then
+	WHILE_FLAG=true
+	while $WHILE_FLAG; do
+		read -p "reinstall nix binary [y/N]: " INPUT
+		if [[ -z "$INPUT" ]]; then
+			INPUT="N"
+		fi
+		case "$INPUT" in
+			"y" | "Y")
+				WHILE_FLAG=false;;
+			"n" | "N")
+				echo "ok. bye!"
+				exit 0
+				;;
+			*)
+				echo -e "invalid input\n" >&2;;
+		esac
+	done
+fi
+
+# install static binary
+# TODO: check if nix version is valid
+mkdir -p "$INSTALL_DIR"
+curl -L "https://hydra.nixos.org/job/nix/maintenance-$VERSION/buildStatic.x86_64-linux/latest/download-by-type/file/binary-dist" > "$BINARY"
+chmod u+x "$BINARY"
+
+PATH_LINE="PATH=$INSTALL_DIR:\$PATH"
+CONFIG_FILE="$HOME/.bashrc"
+case "$(basename $SHELL)" in
+	"zsh")
+		CONFIG_FILE="$HOME/.zshrc";;
+	"fish")
+		PATH_LINE="fish_add_path $INSTALL_DIR"
+		CONFIG_FILE="$HOME/.config/fish/config.fish"
+		;;
+	*)
+		echo "This install script only works with zsh, bash and fish." >&2
+		exit 1
+		;;
+esac
+
+if ! grep "$PATH_LINE" "$CONFIG_FILE" 1>/dev/null; then
+	echo "updating PATH ..."
+	echo "$PATH_LINE" | tee --append "$CONFIG_FILE"
+	echo "updated PATH"
+fi
